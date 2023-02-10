65ab94a7fc2cfecff8bc046870015b27a380eee2
giogio165 <ohjiiiii@gmail.com>
2023-02-10 17:13:11 +0900

    antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
+import React from 'react';
+import { Button, Space } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './main.js';
-
+import  {Switch,Route, Link, useHistory} from 'react-router-dom'
+import UploadPage from './upload';
+import ProductPage from './product';
+import { DownloadOutlined } from '@ant-design/icons';
 
 function App() {
+  const history = useHistory();
   return (
     <div>
-      <MainPage />
-    </div>
+       <div id="header">
+          <div id="header-area">
+            <Link to= "/">
+            <img src="/images/icons/logo.png" /> 
+            {/* 처음에 로고 안 떴었는데 '/' 붙여서 절대 경로로 소스처리를 해야함 */}
+            </Link>
+            <Button 
+            size='large'
+            onClick={function(){
+              history.push('/upload');
+              // 링크 사용안하고 페이지 이동하는 새로운 방법  useHistory
+            }}
+            icon= {<DownloadOutlined />}
+            >상품 업로드</Button>
+          </div> 
+        </div> 
 
+        <div id="body">
+          <Switch>
+            <Route exact={true} path="/">
+              <MainPage />
+            </Route>
+            <Route exact={true} path="/products/:id"> 
+              <ProductPage />
+            </Route>
+            <Route exact={true} path="/upload">
+              <UploadPage />
+            </Route>
+          </Switch>
+        </div>
+        <div id="footer"></div>
+    </div>
   ); 
 } 
 
--- src/main.js/index.js
+++ src/main.js/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css'
 import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 function MainPage(){   
     const [products, setProducts] = React.useState([]);   // products 는 기본적으로 배열이므로 useState 안에 빈 배열을 넣어준다. 
@@ -17,44 +18,35 @@ function MainPage(){
     
     return (
         <div>
-            <div id="header">
-                <div id="header-area">
-                <img src="images/icons/logo.png"/>
-                </div> 
-            </div> 
-            <div id="body">
             <div id="banner">
                 <img src="images/banners/banner1.png" />
             </div>
             <h1>판매 되는 상품들</h1>
             <div id="product-list">
-                {
-                    products.map(function(product, index){
-                        return (
-                            <div className="product-card">
+                {products.map(function(product, index){
+                    return (
+                        <div className="product-card">
+                            <Link className = "product-link" to = {`/products/${[[product.id]]}`}>
                                 <div>
                                     <img className="product-img" src={product.imgUrl}/>
                                 </div>
                                 <div className="product-content">
-                                <span className="product-name">{product.name}</span>
-                                <span className="prod uct-price">{product.price}</span>     
-                                <div className="product-seller">
-                                    <img className="product-avatar"src= "images/icons/avatar.png"/>
-                                    <span>{product.seller}</span>
+                                    <span className="product-name">{product.name}</span>
+                                    <span className="prod uct-price">{product.price}</span>     
+                                    <div className="product-seller">
+                                        <img className="product-avatar"src= "images/icons/avatar.png"/>
+                                        <span>{product.seller}</span>
+                                    </div>
                                 </div>
-                                </div>
-                            </div>
-                        ) 
-                    })
+                            </Link>
+                        </div>
+                    ); 
+                })
                 }
                 
             </div>
-             </div>
-             <div id="footer"></div>
         </div>
     );
 }
-
-
-
+ 
 export default MainPage;  
